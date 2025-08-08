87b299ad4ba6f11f6ce68db8700f6b4ccfafe9c6
dr-akoto <dr.akotoosei@gmail.com>
2025-08-08 16:45:52 +0000

    Implement inheritance demo with Animal base class and Dog/Cat derived classes

---
+++ Dog.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace DCIT318Assignment2
+{
+    // Derived class demonstrating inheritance and method overriding
+    public class Dog : Animal
+    {
+        // Constructor calling the base class constructor (constructor chaining)
+        public Dog(string name) : base(name)
+        {
+            // Constructor chaining calls the base (Animal) constructor
+        }
+
+        // Override the MakeSound method (polymorphism)
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} says: Bark!");
+        }
+    }
+}
